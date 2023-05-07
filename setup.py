0f9727b4622d239fe6051395bfd49ba1dc55e4e9
mjrs33 <koki6839@gmail.com>
2023-05-07 17:16:18 +0900

    update version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name = "matview",
-    version = "0.0.1",
+    version = "0.1.0",
     packages=["matview"],
     install_requires=install_requires
 )
